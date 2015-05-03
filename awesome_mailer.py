3c7ed96088939d37fc6180cbc6f278235b88df3a
Mathias Bak <bufas7@gmail.com>
2015-05-03 21:44:22 +0200

    Move stuff around

--- awesome_mailer.py
+++ awesome_mailer.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return send_from_directory('../client', 'index.html')
+  return send_from_directory('static', 'index.html')
 
 @app.route('/send_mail', methods=['POST'])
 def send_mail():
@@ -18,7 +18,7 @@ def send_mail():
 
 @app.route('/<path:filename>')
 def resources(filename):
-  return send_from_directory('../client', filename)
+  return send_from_directory('static', filename)
 
 if __name__ == '__main__':
   app.run(debug=True)
